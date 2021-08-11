a1389b0724c50d9210e74f41e51bbd57b2459880
Zeke Binion <zeke@binion.io>
2021-08-11 12:21:48 -0400

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Zeke Binion on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
